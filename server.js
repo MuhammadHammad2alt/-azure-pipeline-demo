c71b845e21d0f6d8f66dd0ee446e47663090e38f
M.Hammad <hammadshafique133@gmail.com>
2026-06-22 20:56:10 +0500

    Initial project setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Automation Successful! CI/CD Pipeline is Live.</h1>');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
