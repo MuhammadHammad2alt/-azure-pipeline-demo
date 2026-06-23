20d146c3647ad0bce703cf4268c59ca14841a4c0
M.Hammad <hammadshafique133@gmail.com>
2026-06-23 15:37:38 +0500

    Update homepage heading text

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Automation Successful! CI/CD Pipeline is Live.</h1>');
+    res.send('<h1>Amazing! My automated CI/CD Pipeline updated this website with zero downtime! 🚀</h1>');
+
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
